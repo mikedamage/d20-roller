f5edc2a876f917b67e60edcf8e87c1364bd0fce6
Mike Green <mike.is.green@gmail.com>
2015-04-14 11:37:01 -0400

    Finished ability score generation code in Roller class

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 /* jshint node: true */
 
+// [todo] - Allow for more complex expressions, i.e. "5d6 + 4d8 + 32"
+
 'use strict';
 
-var _ = require('lodash');
+var _      = require('lodash');
+var Random = require('random-js');
 
-var NDN_PATTERN = /(\d+)[dD](\d+)\+?(\d+)?/;
+var NDN_PATTERN = /(\d+)[dD](\d+)\s*\+?\s*(\d+)?/;
 var DEFAULTS    = {
-  pattern: NDN_PATTERN
+  pattern: NDN_PATTERN,
+  engine:  Random.engines.nativeMath
 };
 
 var Roller = function Roller(opts) {
@@ -16,11 +20,64 @@ var Roller = function Roller(opts) {
   }
 
   this.options = _.assign({}, DEFAULTS, opts);
-
-  this.prototype.roll = function roll(diceString) {
-    var matches = diceString.match(this.options.pattern);
-  };
-
 };
+
+Roller.prototype._rollDice = function _rollDice(dice, sides, rerollOnes) {
+  var results = Random.dice(sides, dice)(this.options.engine);
+
+  if (rerollOnes === true && _.includes(results, 1)) {
+    var filtered = _.reject(results, function(n) { return n === 1; });
+    var diff     = results.length - filtered.length;
+    var newInts  = this._rollDice(diff, sides, true);
+    results      = filtered.concat(newInts);
+  }
+
+  return results;
+};
+
+Roller.prototype.roll = function roll(diceString) {
+  var matches = diceString.match(this.options.pattern);
+
+  if (!matches) {
+    throw new Error('Invalid dice string. Must be in the form of "NdN [+ N]"');
+  }
+
+  if (matches.length === 4) {
+    var dice    = matches[1];
+    var sides   = matches[2];
+    var bonus   = _.isNumber(matches[3]) ? matches[3] : 0;
+    var results = this._rollDice(dice, sides);
+
+    return {
+      results: results,
+      total: _.sum(results)
+    };
+  }
+
+  throw new Error('Parse error');
+};
+
+Roller.prototype.abilityScore = function() {
+  var results = this._rollDice(4, 6, true);
+
+  results.sort().shift();
+
+  return {
+    results: results,
+    total: _.sum(results)
+  };
+};
+
+Roller.prototype.abilityScores = function() {
+  var i;
+  var results = [];
+
+  for (i = 0; i < 6; i++) {
+    results.push(this.abilityScore());
+  }
+
+  return results;
+};
+
 
 module.exports = Roller;
